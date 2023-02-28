791d4b4bcd1b4cb810911ae6cb8bb9edc357d442
bpena707 <bpena.610@gmail.com>
2023-02-28 13:05:00 -0800

    message inside icon
    grabbed the message data to display the last bit of text next to icon if the screen is expanded. useEffect to check if chat is active

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,5 +1,12 @@
+import { db } from "@/firebase"
 import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline"
+import { collection, orderBy, query } from "firebase/firestore"
+import { useSession } from "next-auth/react"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import { useCollection } from "react-firebase-hooks/firestore"
 
 type Props = {
     id: string
@@ -8,11 +15,34 @@ type Props = {
 function ChatRow({id}: Props) {
 
     // pathname to highlight the chat that Im in 
-    
+    const pathname = usePathname() //helps read the current url pathname 
+    const router = useRouter //change routes inside client 
+    const { data: session } = useSession() // used to check if someone is logged in 
+    const [active, setActive] = useState(false) //see if user is active 
+
+    // if chat im in is active i need the messages call to useCollection to  query it
+    // ordered by created at date. collection from firestore and the paths
+    const messages = useCollection(
+        query(
+            collection(db, "users", session?.user?.email!, "chats", id, "messages"),
+            orderBy("createdAt", "asc")
+        )
+    )
+
+    // useEffect to determine if chat is active or not 
+    useEffect(() => {
+        if (!pathname) return 
+
+        setActive(pathname.includes(id))
+    },[pathname])
+
   return ( 
     <Link href={`/chat/${id}`} className={`chatRow justify-center-center`} >
         <ChatBubbleLeftIcon className="h-5 w-5" />
-        <p className="flex-1 hidden md:inline-flex truncate">New Chat</p>
+        <p className="flex-1 hidden md:inline-flex truncate">
+            {/* pull the last bit of text from the chat or say new chat  */}
+            {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
+        </p>
         <TrashIcon className="h-5 w-5 text-gray-700 hover:text-red-700" />
 
     </Link>
