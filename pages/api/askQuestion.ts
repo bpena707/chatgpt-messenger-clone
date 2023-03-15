ee68e67c6ab0d59663b5da509675a3f640460cfb
bpena707 <bpena.610@gmail.com>
2023-03-14 22:15:02 -0700

    api file
    created the api file which will utilize the chatGPT api to query data and pass it into the chat input.

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { query } from 'firebase/firestore';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -9,5 +10,23 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+
+    // destructiing what we send to the ChatInput as the request body 
+    const{ prompt,chatId, model,session } =req.body
+
+    // fail safes if prompt and id are not passed
+    if(!prompt) {
+        res.status(400).json({answer: "Please provide a prompt!"});
+        return
+    }
+
+    if(!chatId){
+        res.status(400).json({answer: "Please provide a valid chat ID!"});
+        return 
+    }
+
+    // ChatGPT Query
+    const response = await query(prompt, chatId, model)
+
   res.status(200).json({ name: 'John Doe' })
 }
