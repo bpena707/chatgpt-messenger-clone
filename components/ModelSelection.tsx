f83c0b57102492f0ebfeff0e1b76d96267f938ab
bpena707 <bpena.610@gmail.com>
2023-03-22 13:14:45 -0700

    SWR
    implementing the SWR functionality for model selection

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+'use client'
+import useSWR from "swr";
+import Select from "react-select";
+
+//fectch to call api engines then pass json response 
+const fetchModels = () => fetch("api/getEngines").then((res) => res.json())
 
 function ModelSelection() {
+    const { data: models, isLoading } = useSWR("models, fetchModels")
   return (
-    <div>ModelSelection</div>
+    <div>
+        <Select
+            className="mt-2"
+            options={models?.modelOptions}
+            // defaultValue={model}
+            // placeholder={model}
+            isSearchable
+            isLoading={isLoading}
+            menuPosition="fixed"
+            classNames={{
+                control: (state) => "bg-[#434654] border-[#434654]"
+            }}
+        />
+    </div>
   )
 }
 
